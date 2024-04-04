3548a39639bdd3760a88076d1443dff8f96f5e98
DaisyAram <daisyarama03@gmail.com>
2024-04-04 02:50:37 -0700

    web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,14 +9,14 @@ from datetime import datetime
 
 
 def do_pack():
-    """generating an archive on web_static folder """
+    """generating an archive on web_static folder """    
 
 
-     time = datetime.now()
-     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-     local('mkdir -p versions')
-     create = local('tar -cvzf versions/{} web_static'.format(archive))
-     if create is not None:
-         return archive
-     else:
-         return None
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
