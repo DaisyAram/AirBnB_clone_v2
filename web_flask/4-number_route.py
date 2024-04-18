f8726efe7072410d3f4d547d349d6697f2044c01
DaisyAram <daisyarama03@gmail.com>
2024-04-18 13:20:12 +0000

    flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+   """ root route"""
+   return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+   """ hbnb route"""
+   return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+   """c is fun"""
+   return 'C, {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def Pythoniscool(text='is cool'):
+   """pythonismagic"""
+   return 'Python, {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+   """integer number"""
+   return '{} is a number'.format(n)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
