13217492cc9fb40025c25a5a7f27859a6a7143f9
DaisyAram <daisyarama03@gmail.com>
2024-04-19 00:49:37 -0700

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-from flask import Flask
+"""starts flask web aplication"""
 
+
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """prints hello hbn"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hnbn():
+def hbnb():
+    """display “HBNB”"""
     return 'HBNB'
 
 
