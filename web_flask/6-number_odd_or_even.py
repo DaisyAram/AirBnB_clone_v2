691c4145b562e5132a9ad692280d7c424765df26
DaisyAram <daisyarama03@gmail.com>
2024-04-19 01:56:45 -0700

    6. Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-from flask import Flask
+"""starts a Flask web application"""
 
+from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-   """ root route"""
-   return 'Hello HBNB!'
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hnbn():
-   """ hbnb route"""
-   return 'HBNB'
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-   """c is fun"""
-   return 'C, {}'.format(text.replace('_', ' '))
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def Pythoniscool(text='is cool'):
-   """pythonismagic"""
-   return 'Python, {}'.format(text.replace('_', ' '))
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes=False)
-def number(n):
-   """integer number"""
-   return '{} is a number'.format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def intnumber(n):
+    """ display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
-def integer(n):
+@app.route('/number_template/<int:n>',strict_slashes=False)
+def int(n):
    """display a HTML page only if n is an integer"""
-   return render_template('5-number.html', number=n)
-
+   return render_template('5-number.html', number=n) 
 
 @app.route('/number_odd_or_even/<n>', strict_slashes=False)
 def number_odd_or_even(n):
@@ -46,4 +47,4 @@ def number_odd_or_even(n):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
